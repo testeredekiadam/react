94c40a1582d3e10172388cc02965568b008acee8
testeredekiadam <erkolfirat@gmail.com>
2023-03-02 17:51:57 +0100

    dynamic style

--- first-app/src/components/NewExpense/NewExpense.js
+++ first-app/src/components/NewExpense/NewExpense.js
@@ -12,15 +12,20 @@ const NewExpense = (props) => {
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        setIsEditing(false);
     };
 
-    const startEditing = () => {
+    const startEditingHandler = () => {
         setIsEditing(true);
-    }
+    };
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    };
 
     return <div className="new-expense">
-        {!isEditing && <button onClick={startEditing}>Add New Expense</button>}
-        {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>}
+        {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+        {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}/>}
     </div>
 };
 
