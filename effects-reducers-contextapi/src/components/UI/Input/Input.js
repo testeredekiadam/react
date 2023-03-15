327d143c0a9bfba0c6c17ab5a6db79cd66fda659
testeredekiadam <erkolfirat@gmail.com>
2023-03-15 18:32:01 +0100

    contextApi

---
+++ effects-reducers-contextapi/src/components/UI/Input/Input.js
@@ -0,0 +1,31 @@
+import React, { useRef, useEffect } from "react";
+
+import classes from "./Input.module.css";
+
+const Input = (props) => {
+    const inputRef = useRef();
+
+    useEffect(() => {
+        inputRef.current.focus();
+    }, []);
+
+    return (
+    <div
+        className={`${classes.control} ${
+        props.isValid === false ? classes.invalid : ''
+        }`}
+    >
+        <label htmlFor={props.id}>{props.label}</label>
+        <input
+        ref={inputRef}
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+        />
+    </div>
+            )
+};
+
+export default Input;
