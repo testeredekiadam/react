64bcdc6d59040d2092695498186b30b3d3ee4727
testeredekiadam <erkolfirat@gmail.com>
2023-03-24 03:53:59 +0100

    input bar

--- food-app/src/components/Meals/AvailableMeals.js
+++ food-app/src/components/Meals/AvailableMeals.js
@@ -31,7 +31,7 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = (props) => {
 
-    const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+    const mealsList = DUMMY_MEALS.map((meal) => <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
 
     return (
         <section className={classes.meals}>
