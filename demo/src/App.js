b1e799553a682ab11bdd5650520fe5df59825b6e
testeredekiadam <erkolfirat@gmail.com>
2023-04-06 21:35:00 +0200

    demo & ignore node modules

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
+import Button from "./components/UI/Button/Button";
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Hi there!</h1>
-    </div>
-  );
+
+    const [isShown, setIsShown] = useState(true);
+
+    const showHandler = () => {
+        //setIsShown(isShown ? false : true);
+        setIsShown((prevIsShown) => !prevIsShown);
+    };
+
+    return (
+        <div className="app">
+            <h1>Hi there!</h1>
+            {isShown && <p>This is new!</p>}
+            <Button onClick={showHandler} disabled={false}>Click Here</Button>
+        </div>
+    );
 }
 
 export default App;
