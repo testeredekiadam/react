a87f2eb6a83620a92c81e1442e948d731286f815
testeredekiadam <erkolfirat@gmail.com>
2023-04-07 21:33:50 +0200

    Output & Paragraph

--- demo/src/App.js
+++ demo/src/App.js
@@ -2,20 +2,23 @@ import React, { useState } from 'react';
 
 import './App.css';
 import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
 
-    const [isShown, setIsShown] = useState(true);
+    const [isShown, setIsShown] = useState(false);
 
     const showHandler = () => {
         //setIsShown(isShown ? false : true);
         setIsShown((prevIsShown) => !prevIsShown);
     };
 
+    console.log('App RUNNING');
+
     return (
         <div className="app">
             <h1>Hi there!</h1>
-            {isShown && <p>This is new!</p>}
+            <DemoOutput show={isShown}/>
             <Button onClick={showHandler} disabled={false}>Click Here</Button>
         </div>
     );
