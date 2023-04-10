8b3b7c7c3af0ab6685e079992092d70be91ce497
testeredekiadam <erkolfirat@gmail.com>
2023-04-11 00:19:33 +0200

    firebase url

--- custom-hooks-2/src/App.js
+++ custom-hooks-2/src/App.js
@@ -13,7 +13,7 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
+        'https://react-http-c144c-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'
       );
 
       if (!response.ok) {
